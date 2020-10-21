cf78927669c6f666eaca2c54ff858cb0b8851a99
Lev Michael <lev.choubine@gmail.com>
2020-10-21 15:36:45 -0400

    [js] starwars object code

---
+++ iphone.js
@@ -0,0 +1,10 @@
+const iphone = {
+  image: 'path to where the image is located',
+  length: '11:28',
+  title: 'iPhone12 Unboxing Experience + MagSafe ...',
+  username: 'Marques Brownlee',
+  views: '5.5 million',
+  uploaded: 15
+};
+
+console.log(iphone.title);
---
+++ starwars.js
@@ -0,0 +1,10 @@
+const starwars = { 
+    title: 'Starwars the Mandalorian',
+    studio: 'Disney',
+    channel: 'Starwars',
+    length: '1 minute',
+    views: '1M views',
+    released: '1 day ago'
+}
+
+console.log(starwars.length);
